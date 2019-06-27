b449b45e56e8bafb89ba0b99e05fc6441f5f35d3
Matteo Manica <drugilsberg@gmail.com>
2019-06-27 08:43:36 +0200

    Updated upfp/iterator.py and upfp/tests/utils.py.
    Fixing code style.
    
    Signed-off-by: Matteo Manica <drugilsberg@gmail.com>

--- upfp/tests/utils.py
+++ upfp/tests/utils.py
@@ -8,28 +8,28 @@ class TestFileContent:
     Create a temporary file with a given content.
 
     Inspired by: https://stackoverflow.com/a/54053967/10032558.
-    """                                                                                                
+    """                                                                         
     
     def __init__(self, content: str) -> None:
         """
         Initialize the file with a content.
-        
+ 
         Args:
             content (str): content of the file.
         """
         self.file = tempfile.NamedTemporaryFile(mode='w', delete=False)
-        with self.file as fp:                                                                                            
-            fp.write(content)                                                                                            
+        with self.file as fp: 
+            fp.write(content) 
 
-    @property                                                                                                           
+    @property                                                                                            
     def filename(self):
-        """Get the name of the file."""                                                                                            
-        return self.file.name                                                                                           
+        """Get the name of the file.""" 
+        return self.file.name                                                                                  
 
     def __enter__(self):
-        """Enter the `with` block."""                                                                                              
-        return self                                                                                                     
+        """Enter the `with` block.""" 
+        return self
 
-    def __exit__(self, type, value, traceback):
-        """Exit the `with` block."""                                                                    
+    def __exit__(self, a_type, value, traceback):
+        """Exit the `with` block."""
         os.unlink(self.filename)
